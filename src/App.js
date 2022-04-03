085c9e559ffedf3f6241f51de9216f1b9aa86ab5
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2022-04-03 23:40:58 +0900

    すごい！使いやすい！見やすい！

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-success btn-outline">Hello daisyUI</button>
+    </div>
+  );
 }
